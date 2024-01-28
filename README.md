8c7d30f72fbb160b2c2f774ecc4359c83c014b1b
gustavogss <gustavogss.dev@gmail.com>
2024-01-28 02:51:11 -0300

    feat/first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Gerenciador de Tarefas :
+- Aplicação tipo CRUD para gerenciar tarefas
+
+## Tecnologias utilizadas :robot:
+- Python
+
+## Features :
+1. Adicionar tarefa
+2. Listar tarefas
+3. Atualizar tarefa
+4. Completar tarefa
+5. Remover tarefa
---
+++ menu.py
@@ -0,0 +1,17 @@
+while True:
+    print("\n Menu do Gerenciador de Tarefas: ")
+    print("1. Adicionar tarefa")
+    print("2. Listar tarefas")
+    print("3. Atualizar tarefa")
+    print("4. Completar tarefa")
+    print("5. Remover tarefa")
+    print("6. Sair")
+
+    opcao = input("Escolha uma opção:\n ")
+   
+   
+    if opcao == "6" :
+        break
+    print("Programa finalizado")
+
+
